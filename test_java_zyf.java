12bcb0e13906bfdd49346101bc2789a43b7f3a03
Yokey <zyf6@outlook.com>
2020-03-30 11:52:59 +0800

    Update test_java_zyf.java

--- test_java_zyf.java
+++ test_java_zyf.java
@@ -2,6 +2,7 @@ import Code.algs4.StdOut;
 
 public class test_java_zyf {
     public static void main(String[] args) {
-        StdOut.print("This is a test");
+        StdOut.println("This is a test");
+        StdOut.println("This is ZYF!");
     }
 }
