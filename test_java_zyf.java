7d1946e99460924a6ecf6d19c53233d9003a75be
Yokey <zyf6@outlook.com>
2020-03-30 12:14:44 +0800

    冲突测试

--- test_java_zyf.java
+++ test_java_zyf.java
@@ -1,10 +1,13 @@
+import Code.algs4.StdIn;
 import Code.algs4.StdOut;
 
 public class test_java_zyf {
     public static void main(String[] args) {
         StdOut.println("This is a test");
         StdOut.println("This is ZYF!");
+        String x = StdIn.readLine();
+        StdOut.printf("You just input: %s\n", x);
         StdOut.println("This is LQQ!");
-        StdOUt.println("hello");
+        StdOut.println("hello");
     }
 }
