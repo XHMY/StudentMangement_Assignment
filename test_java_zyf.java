05641e0a316669b81a0b31b0182c57ecbee02208
Lqq-kk <62865213+Lqq-kk@users.noreply.github.com>
2020-03-30 12:02:44 +0800

    Lqq-kk

--- test_java_zyf.java
+++ test_java_zyf.java
@@ -4,5 +4,7 @@ public class test_java_zyf {
     public static void main(String[] args) {
         StdOut.println("This is a test");
         StdOut.println("This is ZYF!");
+        StdOut.println("This is LQQ!");
+        StdOUt.println("hello");
     }
 }
