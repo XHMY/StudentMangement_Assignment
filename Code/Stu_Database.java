c2c72a593bb95a3f8f42989701cae45b1b0544bd
Yokey <zyf6@outlook.com>
2020-04-06 07:56:07 +0800

    增加按学号顺序返回学生功能

--- Code/Stu_Database.java
+++ Code/Stu_Database.java
@@ -1,4 +1,5 @@
 import lib.algs4.LinearProbingHashST;
+import lib.algs4.Queue;
 import lib.algs4.RedBlackBST;
 
 public class Stu_Database {
@@ -9,6 +10,24 @@ public class Stu_Database {
     public Stu_Database() {
         stu_num_data = new RedBlackBST<Integer, Student>();
         stu_name_data = new LinearProbingHashST<String, Student>();
+    }
+
+    // 按学号顺序返回所有学生对象
+    public Iterable<Student> all_stu() {
+        Queue<Student> all = new Queue<Student>();
+        for (int num : stu_num_data.keys()) {
+            all.enqueue(stu_num_data.get(num));
+        }
+        return all;
+    }
+
+    // 按学号顺序返回[s,e]区间内的所有学生
+    public Iterable<Student> all_stu(int s, int e) {
+        Queue<Student> all = new Queue<Student>();
+        for (int num : stu_num_data.keys(s, e)) {
+            all.enqueue(stu_num_data.get(num));
+        }
+        return all;
     }
 
     // 通过学号获取一个学生对象
