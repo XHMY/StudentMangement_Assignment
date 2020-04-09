8ed7ea3e6ead20c046d846540a0e9f01f1410b08
Lqq-kk <62865213+Lqq-kk@users.noreply.github.com>
2020-04-09 21:21:45 +0800

    Lqq

--- StudentManagementSystem/src/com/SMS/CommandLineUI.java
+++ StudentManagementSystem/src/com/SMS/CommandLineUI.java
@@ -8,6 +8,9 @@ public class CommandLineUI {
         StudentMangement sys = new StudentMangement();
         StdOut.println("欢迎使用学生信息管理系统（命令行版）");
         StdOut.println("请输入数字选择相应功能:");
+        if(args[0]==null){
+        StdOut.println("1、添加指定学生在指定时间一次缺课记录\n2、录入指定学生在指定时间的缺课记录\n3、修改指定学生在指定时间的缺课记录\n4、查询并打印指定学生全部的缺课记录\n5、查询指定时间段内的全部缺课学生并按缺课次数顺序打印\n输入-1结束程序\n");
+        }
         int chooseF = StdIn.readInt();
     }
 }
